b02e2604fa8d695394c022ff4e0ef3be8ced5bf6
louis <louis@DESKTOP-3LDQ9KV>
2017-03-29 23:53:40 +0200

    oui

--- test.cpp
+++ test.cpp
@@ -12,7 +12,9 @@ using namespace std;
 
 int main(){
 	//NE PAS OUBLIER DE CHANGER NBCOUCHE SINON C MOR LOL
-	Reseau *r = new Reseau({1,2},{1,5,-3});
+	Reseau *r = new Reseau({1,2},{1,5,-3}); // avec liste de poids
+	//Reseau *r = new Reseau({1,2}); // Avec liste des couches
+//	Reseau *r = new Reseau(); // Vide
 	r->affiche();
 	vector <double> in;
 	in.push_back(1);
